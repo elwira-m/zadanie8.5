4456af4bfe988071585c6c88a0a01132b9325826
Elwira Marcinkowska <elwira.marcinkowska@hotmail.com>
2018-02-15 04:52:19 +0100

    add js/

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//script.js
+
+//first version
+
+/*var a = 4,
+	h = 3,
+	triangleArea;
+	
+triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);*/
+
+//second version
+
+var triangleBase = prompt ( 'Enter base of triangle' );
+var triangleHeight = prompt ( 'Enter height of triangle' );
+
+var triangleArea;
+triangleArea = triangleBase*triangleHeight/2;
+//calculate triangle area
+
+console.log('Triangle field is equal to: ' + triangleArea);
